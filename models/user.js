e4930db6b3eb521c4b6f9111fc27ee49debb023e
Benedict <benedictasare2@gmail.com>
2024-10-27 22:23:11 +0000

    show username even though i log in with email and password

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
 });
 
 UserSchema.plugin(passportLocalMongoose, {
