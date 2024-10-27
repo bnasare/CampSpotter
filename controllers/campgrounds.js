c0967e24c4b34921843d1182bc6e014238f2f3f4
Benedict <benedictasare2@gmail.com>
2024-10-27 21:52:49 +0000

    campgrounds controller

---
+++ controllers/campgrounds.js
@@ -0,0 +1,36 @@
+const Campground = require("../models/campground");
+
+module.exports.index = async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.json({ data: campgrounds, message: "success", status: 200 });
+};
+
+module.exports.createCampground = async (req, res) => {
+  const camp = new Campground(req.body);
+  camp.author = req.user._id;
+  await camp.save();
+  res.json({ data: camp, message: "success", status: 200 });
+};
+
+module.exports.showCampground = async (req, res) => {
+  const camp = await Campground.findById(req.params.id)
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("author");
+  res.json({ data: camp, message: "success", status: 200 });
+};
+
+module.exports.updateCampground = async (req, res) => {
+  const { id } = req.params;
+  const updatedCamp = await Campground.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.json({ data: updatedCamp, message: "success", status: 200 });
+};
+
+module.exports.deleteCampground = async (req, res) => {
+  const { id } = req.params;
+  const deletedCamp = await Campground.findByIdAndDelete(id);
+  res.json({ data: deletedCamp, message: "success", status: 200 });
+};
+
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,65 +2,20 @@ const express = require("express");
 const router = express.Router();
 const Campground = require("../models/campground");
 const catchAsync = require("../utils/catchAsync");
+const campgrounds = require("../controllers/campgrounds");
 const { campgroundSchema } = require("../schema");
 const { isLoggedIn, isAuthor, validateSchema } = require("../middleware");
 
 const validateCampground = validateSchema(campgroundSchema);
 
-router.get(
-  "/",
-  catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.json({ data: campgrounds, message: "success", status: 200 });
-  })
-);
+router.get("/", catchAsync(campgrounds.index));
 
-router.get(
-  "/:id",
-  catchAsync(async (req, res) => {
-    const camp = await Campground.findById(req.params.id)
-      .populate({ path: "reviews", populate: { path: "author" } })
-      .populate("author");
-    res.json({ data: camp, message: "success", status: 200 });
-  })
-);
+router.get("/:id", catchAsync(campgrounds.showCampground));
 
-router.post(
-  "/",
-  isLoggedIn,
-  validateCampground,
-  catchAsync(async (req, res, next) => {
-    const camp = new Campground(req.body);
-    camp.author = req.user._id;
-    await camp.save();
-    res.json({ data: camp, message: "success", status: 200 });
-  })
-);
+router.post("/", isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-router.put(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  validateCampground,
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const updatedCamp = await Campground.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.json({ data: updatedCamp, message: "success", status: 200 });
-  })
-);
+router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
 
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.json({ message: "success", status: 200 });
-  })
-);
+router.delete("/:id", isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
