09f706d0736acc25dae0349fa2460e84fd1d0193
Benedict <benedictasare2@gmail.com>
2024-10-28 00:50:17 +0000

    image thumbnail prop

--- models/campground.js
+++ models/campground.js
@@ -2,28 +2,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review");
 
-const CampgroundSchema = new Schema({
-  title: String,
-  price: Number,
-  description: String,
-  location: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  reviews: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Review",
-    },
-  ],
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
 });
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
+
+const CampgroundSchema = new Schema(
+  {
+    title: String,
+    price: Number,
+    description: String,
+    location: String,
+    images: [ImageSchema],
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
   if (doc) {
