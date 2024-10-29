9cf369ab26809509a9da8e098640c111e40f4a76
Benedict <benedictasare2@gmail.com>
2024-10-29 02:34:48 +0000

    mongo atlas integration

--- app.js
+++ app.js
@@ -17,20 +17,36 @@ const LocalStrategy = require("passport-local").Strategy;
 const User = require("./models/user");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
+const MongoDBStore = require("connect-mongo");
+const dbUrl = "mongodb://localhost:27017/yelp-camp";
 
 // Initialize Express app
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/yelp-camp");
+mongoose.connect(dbUrl);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("Database connected");
 });
 
+// Create a session store using connect-mongo
+const store = new MongoDBStore({
+  mongoUrl: dbUrl,
+  secret: "thissession",
+  touchAfter: 24 * 60 * 60,
+});
+
+// Error handling for the session store
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e);
+});
+
 // Session configuration
 const sessionConfig = {
+  store,
+  name: "session",
   secret: "thissession",
   resave: false,
   saveUninitialized: true,
